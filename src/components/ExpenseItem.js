58892f07e81c62cc03682dc06d61f5a88ea113c9
tinuola <24995224+tinuola@users.noreply.github.com>
2022-07-03 21:49:23 -0700

    Passing dynamic data with props

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,16 +1,26 @@
 import './ExpenseItem.css';
 
-function ExpenseItem() {
-  const expenseDate = new Date(2021, 2, 25);
-  const expenseTitle = 'Car Insurance';
-  const expenseAmount = 294.67;
+function ExpenseItem(props) {
+  // const expenseDate = new Date(2021, 2, 25);
+  // const expenseTitle = 'Car Insurance';
+  // const expenseAmount = 294.67;
+
+  const day = props.date.toLocaleString('en-US', { day: '2-digit' });
+  const month = props.date.toLocaleString('en-US', { month: 'long' });
+  const year = props.date.getFullYear();
 
   return (
     <div className='expense-item'>
-      <div>{expenseDate.toISOString()}</div>
+      {/* <div>{props.date.toISOString()}</div> */}
+      {/* Change how dates are displayed */}
+      <div>
+        <div>{month}</div>
+        <div>{year}</div>
+        <div>{day}</div>
+      </div>
       <div className='expense-item__description'>
-        <h2>{expenseTitle}</h2>
-        <div className='expense-item__price'>${expenseAmount}</div>
+        <h2>{props.title}</h2>
+        <div className='expense-item__price'>${props.amount}</div>
       </div>
     </div>
   );
